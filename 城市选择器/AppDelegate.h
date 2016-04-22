7c4493fb22844dafa7586cdf61bdf1a46dfda2f2
jun <584823043@qq.com>
2016-04-22 10:02:39 +0800

    Initial Commit

---
+++ 城市选择器/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  城市选择器
+//
+//  Created by jun on 16/4/22.
+//  Copyright © 2016年 jun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 城市选择器/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  城市选择器
+//
+//  Created by jun on 16/4/22.
+//  Copyright © 2016年 jun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
